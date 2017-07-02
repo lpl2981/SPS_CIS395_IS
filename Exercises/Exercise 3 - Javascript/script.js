2ecc694278f31b27e3f9183f5d5e5719c95d4895
Logan P <logan.porter58@gmail.com>
2017-07-02 09:41:02 -0500

    typo fix

--- Exercises/Exercise 3 - Javascript/script.js
+++ Exercises/Exercise 3 - Javascript/script.js
@@ -2,9 +2,9 @@ var testVariable;
 var isUndefined = (testVariable === undefined);
 var isNull = (testVariable === null);
 var isEmpty = (testVariable === '');
-//checks value type of variable testVariable
+//checks value type of testVariable
 
-document.getElementsById('isUndefined)').innerHTML = isUndefined;
+document.getElementById('isUndefined)').innerHTML = isUndefined;
 document.getElementById('isNull').innerHTML = isNull;
 document.getElementById('isEmpty').innerHTML = isEmpty;
 
