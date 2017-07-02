ac08b063f3ac1e95d73f9f7d2f860408e66142f1
Logan P <logan.porter58@gmail.com>
2017-07-02 09:25:24 -0500

    initial commit - added HTML & .js file for checking value type of variable

---
+++ Exercises/Exercise 3 - Javascript/script.js
@@ -0,0 +1,9 @@
+var testVariable;
+var isUndefined = (testVariable === undefined);
+var isNull = (testVariable === null);
+var isEmpty = (testVariable === '');
+//checks value type of variable testVariable
+
+document.getElementsById('isUndefined)').innerHTML = isUndefined;
+document.getElementById('isNull').innerHTML = isNull;
+document.getElementById('isEmpty').innerHTML = isEmpty;
