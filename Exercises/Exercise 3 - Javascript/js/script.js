6d2474107e1ab03db5454745350a5e7c15844d75
Logan P <logan.porter58@gmail.com>
2017-07-02 09:48:54 -0500

    change to order of commands

--- Exercises/Exercise 3 - Javascript/js/script.js
+++ Exercises/Exercise 3 - Javascript/js/script.js
@@ -4,8 +4,8 @@ var isNull = (testVariable === null);
 var isEmpty = (testVariable === '');
 ///checks value type of testVariable
 
+var testVariable = 'ABC';
+
 document.getElementById('isUndefined').innerHTML = isUndefined;
 document.getElementById('isNull').innerHTML = isNull;
 document.getElementById('isEmpty').innerHTML = isEmpty;
-
-var testVariable = 'ABC';
