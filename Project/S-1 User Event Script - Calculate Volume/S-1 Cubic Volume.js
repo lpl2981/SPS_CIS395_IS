75938d9bb43753bac6b16e53a71e3295d2b21ffa
Logan P <logan.porter58@gmail.com>
2017-08-07 17:39:22 -0500

    additional changes due to new error

--- Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
+++ Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
@@ -1,5 +1,3 @@
-var SEARCHMODULE;
- 
 /**
  *@NApiVersion 2.x
  *@NModuleScope Public
@@ -7,18 +5,20 @@ var SEARCHMODULE;
  */
 
 define(['N/record'], function(record) {
-    function beforeSubmit(scriptContext) {
-        //SEARCHMODULE.load(123);
+    function beforeSubmit(context) {
         log.debug('beforeSubmit Triggered');
-        if (scriptContext.type === scriptContext.UserEventType.CREATE) {
-            var newRecord = scriptContext.newRecord;
-            var oldRecord = scriptContext.oldRecord;
-            var length = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_length_inches} );
-            var height = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_height_inches} );
-            var width = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_width_inches} );
-        newRecord.setValue({
+        if (context.type === context.UserEventType.CREATE)
+            return;
+        var itemRecord = context.newRecord;
+        var oldItemRecord = context.oldRecord;
+        var length = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_length_inches} );
+        var height = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_height_inches} );
+        var width = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_width_inches} );
+        itemRecord.setValue({
             fieldid: 'custitem_nw_is395_item_vol_cub_inches',
             value: length * width * height});
-            };
+        };
+        return {
+            beforeSubmit: beforeSubmit
         };
     });
