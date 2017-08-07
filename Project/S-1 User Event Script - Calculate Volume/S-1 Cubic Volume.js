110f49854140ca3788fb1351f87e740198a3545f
Logan P <logan.porter58@gmail.com>
2017-08-07 17:28:10 -0500

    changes due to error

--- Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
+++ Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
@@ -5,35 +5,20 @@ var SEARCHMODULE;
  *@NModuleScope Public
  *@NScriptType UserEventScript
  */
-define(['N/search'], runUserEvent);
 
-function runUserEvent(search) {
-	SEARCHMODULE = search;
-	var returnObj = {};
-	returnObj.beforeLoad = beforeLoad;
-	returnObj.afterSubmit = afterSubmit;
-	returnObj.beforeSubmit = beforeSubmit;
-	return returnObj;
-}
- 
-function beforeSubmit(context) {
-	//SEARCHMODULE.load(123);
-	log.debug('beforeSubmit Triggered');
-	context.newRecord;
-	context.oldRecord;
-	context.type;
-	return;
-}
-
-function Volume(context) {
-    //Calculates volume & writes to field before submit to server
-    log.debug('beforeSubmit Triggered');
-    var record = context.currentRecord;
-    var length = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_length_inches} );
-    var height = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_height_inches} );
-    var width = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_width_inches} );
-    currentRecord.setValue({
-        fieldid: 'custitem_nw_is395_item_vol_cub_inches',
-        value: length * width * height
+define(['N/record'], function(record) {
+    function beforeSubmit(scriptContext) {
+        //SEARCHMODULE.load(123);
+        log.debug('beforeSubmit Triggered');
+        if (scriptContext.type === scriptContext.UserEventType.CREATE) {
+            var newRecord = scriptContext.newRecord;
+            var oldRecord = scriptContext.oldRecord;
+            var length = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_length_inches} );
+            var height = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_height_inches} );
+            var width = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_width_inches} );
+        newRecord.setValue({
+            fieldid: 'custitem_nw_is395_item_vol_cub_inches',
+            value: length * width * height});
+            };
+        };
     });
-}
