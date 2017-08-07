dc9785eaa816c3f747f8af211da277524347550f
Logan P <logan.porter58@gmail.com>
2017-08-07 16:44:06 -0500

    Removed examples for Before Load and After Submit

--- Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
+++ Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
@@ -9,30 +9,11 @@ define(['N/search'], runUserEvent);
 
 function runUserEvent(search) {
 	SEARCHMODULE = search;
- 
 	var returnObj = {};
 	returnObj.beforeLoad = beforeLoad;
 	returnObj.afterSubmit = afterSubmit;
 	returnObj.beforeSubmit = beforeSubmit;
 	return returnObj;
-}
- 
-function beforeLoad(context) {
-	//SEARCHMODULE.load(123);
-	log.debug('beforeLoad Triggered');
-	context.newRecord;
-	context.type;
-	context.form;
-	return;
-}
- 
-function afterSubmit(context) {
-	//SEARCHMODULE.load(123);
-	log.debug('afterSubmit Triggered');
-	context.newRecord;
-	context.oldRecord;
-	context.type;
-	return;
 }
  
 function beforeSubmit(context) {
@@ -54,6 +35,5 @@ function Volume(context) {
     currentRecord.setValue({
         fieldid: 'custitem_nw_is395_item_vol_cub_inches',
         value: length * width * height
-    
-    })
+    });
 }
