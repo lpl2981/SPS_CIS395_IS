f362f86227c0f1a99aa0f03b8ebc017a8f698ed6
Logan P <logan.porter58@gmail.com>
2017-08-22 09:32:58 -0500

    S-2 script fixes for syntax, add forloop

--- Project/S-1 User Event Script - Calculate Volume/S-2 Sales Order Item Calculation.js
+++ Project/S-1 User Event Script - Calculate Volume/S-2 Sales Order Item Calculation.js
@@ -9,3 +9,15 @@ var noLines = newRecord.getLineCount({
 }); 
 // for each line
 var total = 0;
+for (var index = 0; index < noLines; index++) {
+    total += newRecord.getSublistValue({
+        sublistId: 'item',
+        fieldId: 'custcol_nw_is395_item_cubic_inches',
+        line: index
+    });
+}
+
+newrecord.SetValue({
+    fieldId: 'custbody_nw_is_395_s2_order_space',
+    value: total
+});
