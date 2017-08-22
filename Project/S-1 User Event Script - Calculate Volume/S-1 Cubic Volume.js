375aeaefcb054f7dc19ca6882d147c57ccef5db8
Logan P <logan.porter58@gmail.com>
2017-08-22 09:04:08 -0500

    Changes - fixed syntax & semicolons

--- Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
+++ Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
@@ -4,33 +4,18 @@
  *@NScriptType UserEventScript
  */
 
-define(['N/record'], function(record) {
-    function beforeSubmit(context) {
-        log.debug('beforeSubmit Triggered');
-        if (context.type === context.UserEventType.CREATE)
-            return;
-        var itemRecord = context.newRecord;
-        var oldItemRecord = context.oldRecord;
-        var length = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_length_inches} );
-        var height = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_height_inches} );
-        var width = record.getFieldValue( {fieldid: record.custitem_nw_is395_item_width_inches} );
-        itemRecord.setValue({
-            fieldId: 'custitem_nw_is395_item_vol_cub_inches',
-            value: length * width * height
-            });
-        };
-        return {
-            beforeSubmit: beforeSubmit
-        };
-    })
-
 define([], function() {
     function beforeSubmit(context) {
         var record = context.newRecord; // record.Record object, not "record" module
-        var length = record.getValue({ fieldId: "custitem_nw_is395_item_length_inches"})
-        var width = record.getValue({ fieldId: "custitem_nw_is395_item_width_inches"})
-        var height = record.getValue({ fieldId:"custitem_nw_is395_item_height_inches"})
-    });
+        var length = record.getValue({ fieldId: "custitem_nw_is395_item_length_inches"});
+        var width = record.getValue({ fieldId: "custitem_nw_is395_item_width_inches"});
+        var height = record.getValue({ fieldId:"custitem_nw_is395_item_height_inches"});
+        log.debug('testing calculation', length*width*height),
+        record.setValue({
+            fieldId: custitem_nw_is395_item_vol_cub_inches,
+            value: length * width * height
+        });
+    }
 }
 
 return {
