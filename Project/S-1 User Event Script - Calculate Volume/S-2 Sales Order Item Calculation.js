d88f9a3fecb821fecc7977683b62d5b2d5759135
Logan P <logan.porter58@gmail.com>
2017-08-22 09:39:30 -0500

    adding quantity multiplier

--- Project/S-1 User Event Script - Calculate Volume/S-2 Sales Order Item Calculation.js
+++ Project/S-1 User Event Script - Calculate Volume/S-2 Sales Order Item Calculation.js
@@ -8,7 +8,7 @@ define([], function() {
 
 
 var noLines = newRecord.getLineCount({
-    sublistId: 'item' // from SO record browser
+    sublistId: 'item' // from Sales Order record browser
 }); 
 // for each line
 var total = 0;
@@ -16,12 +16,21 @@ for (var index = 0; index < noLines; index++) {
     total += newRecord.getSublistValue({
         sublistId: 'item',
         fieldId: 'custcol_nw_is395_item_cubic_inches',
+        line: index 
+    });
+    total * newRecord.getSublistValue({
+        sublistId: 'item',
+        fieldId: 'quantity',
         line: index
     });
-}
+    }
 
-newrecord.SetValue({
+
+newRecord.SetValue({
     fieldId: 'custbody_nw_is_395_s2_order_space',
     value: total
-})
+});
+return {
+    beforeSubmit: beforeSubmit
+};
 }); 
