3db343c996400f602b630c221f06ca735f650799
Logan P <logan.porter58@gmail.com>
2017-08-24 11:19:11 -0500

    Added logging

--- Project/S-3 User Event Script - Truck Reservation System/S-3 Truck Reservation System.js
+++ Project/S-3 User Event Script - Truck Reservation System/S-3 Truck Reservation System.js
@@ -6,9 +6,11 @@
 
 define(['N/search'], function() {
 function beforeSubmit(context) {
-    var record = context.newRecord; // loads record object
-    var truckRecord = search.lookupFields({ type: search.Type.customrecord_nw_is395_delivery_trucks, id: record.getValue({ fieldId: 'custbody_nw_is395_delivery_shipment.id' }), columns: ['custrecord_nw_is395_sq_ft', 'custrecord_nw_is395_delivery_truck']
-    });
+    var record = context.newRecord; // loads record object for the Sales Order
+    var truckRecord = search.lookupFields({ type: 'customrecord_nw_is395_delivery_trucks', id: record.getValue({ fieldId: 'custbody_nw_is395_delivery_shipment' }), columns: ['custrecord_nw_is395_sq_ft', 'custrecord_nw_is395_delivery_truck']
+});
+    log.debug('look up fields', JSON.stringify(truckRecord));
+    return
     var spaceAvailable = record.getValue({ fieldId: 'custrecord_nw_is395_sq_ft' });
     var spaceNeeded = record.getValue({ fieldId: 'custbody_nw_is_395_s2_order_space' });
     var truckStatus = record.getValue({ fieldId: 'custrecord_nw_is395_delivery_truck' });
