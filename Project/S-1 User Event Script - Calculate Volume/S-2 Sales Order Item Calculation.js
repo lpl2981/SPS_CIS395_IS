147e2d77006acdf9943cc4cd4932b9d95e9c342c
Logan P <logan.porter58@gmail.com>
2017-08-23 22:10:23 -0500

    script revisions

--- Project/S-1 User Event Script - Calculate Volume/S-2 Sales Order Item Calculation.js
+++ Project/S-1 User Event Script - Calculate Volume/S-2 Sales Order Item Calculation.js
@@ -5,32 +5,31 @@
  */
 
 define([], function() {
+function beforeSubmit(context) {
+    var noLines = newRecord.getLineCount({
+        sublistId: 'item' // from Sales Order record browser
+    }); 
+    // for each line
+    var total = 0;
+    for (var index = 0; index < noLines; index++) {
+        total += newRecord.getSublistValue({
+            sublistId: 'item',
+            fieldId: 'custcol_nw_is395_item_cubic_inches',
+            line: index 
+        });
+        total * newRecord.getSublistValue({
+            sublistId: 'item',
+            fieldId: 'quantity',
+            line: index
+        });
+        }
 
-
-var noLines = newRecord.getLineCount({
-    sublistId: 'item' // from Sales Order record browser
-}); 
-// for each line
-var total = 0;
-for (var index = 0; index < noLines; index++) {
-    total += newRecord.getSublistValue({
-        sublistId: 'item',
-        fieldId: 'custcol_nw_is395_item_cubic_inches',
-        line: index 
+    newRecord.SetValue({
+        fieldId: 'custbody_nw_is_395_s2_order_space',
+        value: total
     });
-    total * newRecord.getSublistValue({
-        sublistId: 'item',
-        fieldId: 'quantity',
-        line: index
-    });
-    }
-
-
-newRecord.SetValue({
-    fieldId: 'custbody_nw_is_395_s2_order_space',
-    value: total
-});
-return {
-    beforeSubmit: beforeSubmit
-};
-}); 
+}
+    return {
+        beforeSubmit: beforeSubmit
+    };
+    }); 
