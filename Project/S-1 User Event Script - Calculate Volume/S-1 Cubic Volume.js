bdf6b897f535e7be136cad9cd4e6b6f905d96cb3
Logan P <logan.porter58@gmail.com>
2017-08-29 17:00:11 -0500

    commit

--- Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
+++ Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
@@ -10,7 +10,7 @@ define([], function () {
     var length = record.getValue({ fieldId: "custitem_nw_is395_item_length_inches" });
     var width = record.getValue({ fieldId: "custitem_nw_is395_item_width_inches" });
     var height = record.getValue({ fieldId: "custitem_nw_is395_item_height_inches" });
-    log.debug('testing calculation', length* width * height)
+    log.debug('testing calculation', length * width * height)
     record.setValue({
       fieldId: 'custitem_nw_is395_item_vol_cub_inches',
       value: length * width * height
