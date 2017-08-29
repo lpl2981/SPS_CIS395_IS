284d2062ffe185f7513e2b96a498fa37280c0815
Logan P <logan.porter58@gmail.com>
2017-08-29 10:58:16 -0500

    Update to S-1
    Added log.debug statements and called N/Record API module

--- Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
+++ Project/S-1 User Event Script - Calculate Volume/S-1 Cubic Volume.js
@@ -4,13 +4,28 @@
  *@NScriptType UserEventScript
  */
 
-define([], function () {
+define(['N/record'], function (volumeCalc) {
   function beforeSubmit(context) {
     var record = context.newRecord; // record.Record OBJECT NOT "record" module
     var length = record.getValue({ fieldId: "custitem_nw_is395_item_length_inches" });
-    var width = record.getValue({ fieldId: "custitem_nw_is395_item_width_inches" });
+    log.debug({
+      title: 'length',
+      details: 'Value of Length: ' + typeof(length)
+    });
+    var width = record.getValue({ fieldId: "custitem_nw_is395_width_inches" });
+      log.debug({
+      title: 'width',
+      details: 'Value of Width: ' + typeof(width)
+    });
     var height = record.getValue({ fieldId: "custitem_nw_is395_item_height_inches" });
-    log.debug('testing calculation', length* width * height)
+      log.debug({
+      title: 'height',
+      details: 'Value of Height: ' + typeof(height)
+    });
+    log.debug({
+    title: 'Testing Calculation', 
+    details: 'Value of Item Volume: ' + (length * width * height)
+    });
     record.setValue({
       fieldId: 'custitem_nw_is395_item_vol_cub_inches',
       value: length * width * height
