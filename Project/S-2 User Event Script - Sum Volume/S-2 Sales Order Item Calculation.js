7ec24449532a6438b9fd6adbe9b1e8af53cfe886
Logan P <logan.porter58@gmail.com>
2017-08-29 18:49:08 -0500

    Update to indents

--- Project/S-2 User Event Script - Sum Volume/S-2 Sales Order Item Calculation.js
+++ Project/S-2 User Event Script - Sum Volume/S-2 Sales Order Item Calculation.js
@@ -5,36 +5,36 @@
  */
 
 define([], function() {
-function beforeSubmit(context) {
-    var newRecord = context.newRecord;
-    var noLines = newRecord.getLineCount({
-        sublistId: 'item' // from Sales Order record browser
-    }); 
-    // for each line
-    var total = 0;
-    for (var index = 0; index < noLines; index++) {
-        total += newRecord.getSublistValue({
-            sublistId: 'item',
-            fieldId: 'custcol_nw_is395_item_cubic_inches',
-            line: index 
-        });
-    log.debug({
-      title: 'value of Total',
-      details: 'Value of Total: ' + total + typeof(total)
-    });
-        total *= newRecord.getSublistValue({
-            sublistId: 'item',
-            fieldId: 'quantity',
-            line: index
-        });
+    function beforeSubmit(context) {
+        var newRecord = context.newRecord;
+        var noLines = newRecord.getLineCount({
+            sublistId: 'item' // from Sales Order record browser
+        }); 
+        // for each line
+        var total = 0;
+        for (var index = 0; index < noLines; index++) {
+            var cubicInches = newRecord.getSublistValue({
+                sublistId: 'item',
+                fieldId: 'custcol_nw_is395_item_cubic_inches',
+                line: index 
+            });
+            log.debug({
+            title: 'value of Total',
+            details: 'Value of Total: ' + total + typeof(total)
+            });
+            var quantity = newRecord.getSublistValue({
+                sublistId: 'item',
+                fieldId: 'quantity',
+                line: index
+            });
+            total += (quantity * cubicInches);
         }
-
-    newRecord.setValue({
-        fieldId: 'custbody_nw_is_395_s2_order_space',
-        value: parseInt(total/1728)
-        //value: Math.round(total/1728)
-    });
-}
+        newRecord.setValue({
+            fieldId: 'custbody_nw_is_395_s2_order_space',
+            value: parseInt(total/1728)
+            //value: Math.round(total/1728)
+        });
+    }
     return {
         beforeSubmit: beforeSubmit
     };
